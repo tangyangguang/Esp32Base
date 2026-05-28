a15a7cf065a55c192c315c31674e267c815f8c15
tttabc123 <254030999@qq.com>
2026-05-29 07:41:06 +0800

    Refine app config restart markers

--- examples/full_demo/src/main.cpp
+++ examples/full_demo/src/main.cpp
@@ -294,6 +294,7 @@ void runSelfTest() {
     RUN_SELFTEST("GET", "/esp32base/app-config", nullptr, true, 200, "Save App Config");
     RUN_SELFTEST("GET", "/esp32base/app-config", nullptr, true, 200, "data-group='General'");
     RUN_SELFTEST("GET", "/esp32base/app-config", nullptr, true, 200, "class=\\\"acgroup\\\"");
+    RUN_SELFTEST("GET", "/esp32base/app-config", nullptr, true, 200, "class='tag warn restart'>restart</span>");
     RUN_SELFTEST("GET", "/esp32base/app-config", nullptr, true, 200, "Multiplier");
     RUN_SELFTEST("GET", "/esp32base/app-config", nullptr, true, 200, "Offset");
     RUN_SELFTEST("GET", "/esp32base/app-config", nullptr, true, 200, "Timeout");
